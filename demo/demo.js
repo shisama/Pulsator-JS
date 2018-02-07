63023edd947bca60e0975ff71e21bef8bf8d1001
Masashi Hirano <cherrydog07@gmail.com>
2018-02-07 23:27:47 +0900

    Improve demo page

--- demo/demo.js
+++ demo/demo.js
@@ -7,7 +7,7 @@ document.body.appendChild(el);
 
 window.onclick = function (event) {
   el.style.display = "block";
-  el.style.marginLeft = event.pageX + "px";
-  el.style.marginTop = event.pageY + "px";
+  el.style.marginLeft = event.pageX - 15 + "px";
+  el.style.marginTop = event.pageY - 50 + "px";
   target.start();
 };
