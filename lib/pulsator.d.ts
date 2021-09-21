6da3dbf1d64a2d625b98961e0def07de85afb161
Masashi Hirano <shisama07@gmail.com>
2021-09-22 02:49:30 +0900

    !feat: change constructor interface and add startByMouseEvent (#77)
    BREAKING CHANGE
    
    - remove parentNode from constructor arguments
    - add startByMouseEvent method
    
    Co-authored-by: Masashi Hirano <cherrydog07@gmail.com>

--- lib/pulsator.d.ts
+++ lib/pulsator.d.ts
@@ -1,10 +1,21 @@
+declare type Options = {
+    style: Record<string, string>;
+    duration: number;
+    iterations: number;
+    color: {
+        r: number;
+        g: number;
+        b: number;
+    };
+};
 export default class Pulsator {
     private element;
     private animation;
-    constructor(parent: Element, options?: unknown);
+    constructor(options?: Options);
     start(): void;
     stop(): void;
     pause(): void;
     reverse(): void;
     getElement(): HTMLElement;
 }
+export {};
